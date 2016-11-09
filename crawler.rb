9b3caae266a1cc2be95af7f71020dec5928d95e3
Jiri Peinlich <jiri.peinlich@ingg.com>
2016-11-09 17:32:55 +0000

    preparing for crawling...

--- crawler.rb
+++ crawler.rb
@@ -1,1 +1,23 @@
-puts "Hello!"
+require 'net/http'
+require 'nokogiri'
+
+host = 'www.nhs.uk'
+
+puts "Gathering index urls..."
+source = Net::HTTP.get(host , '/Conditions/Pages/hub.aspx')
+html = Nokogiri::HTML(source)
+urls = html.xpath('//*[@id="haz-mod1"]/ul/li/a/@href').map {|l| "/Conditions/Pages/#{l}"}
+urls = [urls.first]
+
+urls.each do |url|
+	sleep 1
+	puts "Processing #{url}"
+	html = Nokogiri::HTML(Net::HTTP.get(host ,url))
+	html.xpath('//*[@id="haz-mod5"]/div/div/ul/li').each do |section|
+		section_name =  section.xpath('.//a')[0].inner_html
+		section_url = section.xpath('.//a/@href')[0]
+		exit
+	end
+end
+
+
